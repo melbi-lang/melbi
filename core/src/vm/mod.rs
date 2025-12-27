ffa7ee40ed954601755e27a20a987534fd01c3e9
Nilton Volpato <nilton@volpa.to>
2025-12-26 16:20:27 -0800

    Make API submodules private and fix VM re-exports

--- core/src/vm/mod.rs
+++ core/src/vm/mod.rs
@@ -8,8 +8,6 @@ mod instruction_set;
 mod stack;
 mod vm;
 
-pub use self::vm::VM;
-
 pub use array_contains_adapter::ArrayContainsAdapter;
 pub use cast_adapter::CastAdapter;
 pub use code::{Code, LambdaCode, LambdaKind};
@@ -17,5 +15,6 @@ pub use format_str_adapter::FormatStrAdapter;
 pub use function_adapter::FunctionAdapter;
 pub use generic_adapter::GenericAdapter;
 pub use instruction_set::Instruction;
+pub use vm::VM;
 
 pub(crate) use stack::Stack;
