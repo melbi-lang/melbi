39897ab9f5967b437b5876305a9f307f1d58cd5c
Nilton Volpato <nilton@volpa.to>
2026-01-06 22:31:56 -0800

    Add thin-ref support crate
    Introduce ThinRef<'a, T> wrapper (no_std) that wraps a reference and
    provides Deref, Debug, Display, Error, Send and Sync. Includes a no-op
    Drop implementation, Cargo.toml, and a minimal test. Workspace metadata
    and Cargo.lock were updated

---
+++ support/thin-ref/src/lib.rs
@@ -0,0 +1,72 @@
+#![no_std]
+
+use core::{error::Error, fmt, marker::PhantomData, ops::Deref};
+
+pub struct ThinRef<'a, T>
+where
+    T: 'a + ?Sized,
+{
+    inner: &'a T,
+    phantom: PhantomData<&'a T>,
+}
+
+impl<'a, T> ThinRef<'a, T>
+where
+    T: 'a,
+{
+    pub fn new(value: &'a T) -> Self {
+        ThinRef {
+            inner: value,
+            phantom: PhantomData,
+        }
+    }
+}
+
+impl<'a, T> ThinRef<'a, [T]>
+where
+    [T]: 'a,
+{
+    pub fn from_slice(value: &'a [T]) -> Self {
+        ThinRef {
+            inner: value,
+            phantom: PhantomData,
+        }
+    }
+}
+
+impl<'a, T: ?Sized + fmt::Debug> fmt::Debug for ThinRef<'a, T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.inner.fmt(f)
+    }
+}
+impl<'a, T: ?Sized + fmt::Display> fmt::Display for ThinRef<'a, T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.inner.fmt(f)
+    }
+}
+impl<'a, T: ?Sized + Error> Error for ThinRef<'a, T> {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        self.inner.source()
+    }
+}
+impl<'a, T: ?Sized> Deref for ThinRef<'a, T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        self.inner
+    }
+}
+impl<'a, T: ?Sized> Drop for ThinRef<'a, T> {
+    fn drop(&mut self) {}
+}
+
+unsafe impl<T: ?Sized + Send> Send for ThinRef<'_, T> {}
+unsafe impl<T: ?Sized + Sync> Sync for ThinRef<'_, T> {}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn it_works() {
+        assert_eq!(1 + 1, 2);
+    }
+}
